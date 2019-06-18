8225eac406ba7e3525dca75e63e60cd61eda1683
chokoreetosan <mallepalliabhishek@gmail.com>
2019-06-17 21:58:50 -0700

    formatting

--- Account.sol
+++ Account.sol
@@ -1,19 +1,14 @@
 pragma solidity ^0.4.25;
 
 contract Account{
-    address private vault;
-
-    string private password;
+  address private vault;
+  string private password;
   constructor() public{
     vault = msg.sender;
     password = 'password';
   }
-
-
   function withdraw ( string pw, uint howmuch) public {
     require(keccak256(bytes(pw)) == keccak256(bytes(password)),"Not authorized as account holder");
     msg.sender.transfer(howmuch);
-
-
   }
 }
